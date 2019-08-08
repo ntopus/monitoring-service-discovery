8c744902f6b459894ec8bfe6f4676e4cc42db2a4
Eduardo Paim de Mattos Santos <edu_paim@hotmail.com>
2019-08-08 16:14:26 -0300

    Update version.go

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ const ApplicationName = "monitoring"
 var GitCommit string
 
 // The main version number that is being run at the moment.
-const Version = "0.3.0"
+const Version = "test"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
